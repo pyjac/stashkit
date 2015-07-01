50ea0cace90a683095342a27d2ea220430f3d376
Eliemenye, Larry <Larry.Eliemenye@tuitravel.com>
2015-07-01 09:34:33 +0100

    Added front end libraries, code to process ini files

--- conf/index.js
+++ conf/index.js
@@ -1,11 +1,16 @@
 /**
- * Created by tthlex on 26/06/15.
+ * Created by larry Eliemenye
  */
+var ini = require("ini"),
+    fs = require("fs"),
+    path = require("path")
+config = {}
 
-
-module.exports = {
-    database:{
-        url:'localhost',
-        name:'BucketFS'
-    }
-}
+var files = fs.readdirSync(__dirname);
+files.filter(function (file) {
+    return file != "index.js"
+}).forEach(function (file) {
+    var base = path.basename(file, ".ini");
+    config[base] = ini.parse(fs.readFileSync(__dirname + "/" + file, 'utf-8'));
+});
+module.exports = config;
