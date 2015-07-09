f48d4619a8e4835d811c1fbccc61337035d21585
Larry Eliemenye <denachural@gmail.com>
2015-07-10 00:29:04 +0100

    Configured test for travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 var browserify = require('gulp-browserify');
 
 gulp.task('default', function(){
 
+});
+
+
+gulp.task('run:mocha', function(){
+    return gulp.src('./test/**/*.js').pipe(mocha({
+        reporter:'spec'
+    }))
 });
--- test/libs/stashkit-spec.js
+++ test/libs/stashkit-spec.js
@@ -1,3 +1,13 @@
 /**
  * Created by larry Eliemenye
  */
+var expect = require('chai').expect,
+    should = require('chai').should(),
+    asset = require('chai').assert;
+
+describe('stashkit node module', function(){
+    it('should export and instance of Stashkit', function(){
+        var foo = 'hello';
+        foo.should.be.a('string');
+    })
+});
