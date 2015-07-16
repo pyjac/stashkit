e6de7ccd6df294afdc4e90b60bc5a0838c286580
Eliemenye, Larry <Larry.Eliemenye@tuitravel.com>
2015-07-16 09:45:13 +0100

    Some, More UI stuff, Styles, Boilerplate methods

--- core/lib/PassportConfig.js
+++ core/lib/PassportConfig.js
@@ -1,3 +1,10 @@
+//TODO: 1. Install Connect Session
+//TODO: 2. Configure passport local
+//TODO: 3. Configure mongodb to store session ids
+//TODO: 4. Store User Details in session so we can use that detail to log in to mongodb internally to fetch details that require auth
+//TODO: 5. install angular-ui-router bower compoenent
+var passport = require('passport');
 module.exports = function(app){
+
 
 };
--- core/server/routes/Bucket.js
+++ core/server/routes/Bucket.js
@@ -5,9 +5,19 @@
 var express = require('express');
 var router = express.Router();
 var Auth = require('../lib/OAuth');
+var Admin = require('../../lib/database/Admin');
 
 function createBucket(req, res, next){
+    var body = req.body//, params = req.params, headers = req.headers;
+    var data = {
+        db_user:body.db_user,
+        db_name:body.db_name
+    };
 
+    Admin.getBucketList(data, function(err, list){
+        if(err) return res.json(err)
+        return res.json(list);
+    });
 }
 function deleteBucket(req, res, next){
 
@@ -16,7 +26,16 @@ function editBucket(req, res, next){
 
 }
 function getBucket(req, res, next){
+    var body = req.body//, params = req.params, headers = req.headers;
+    var data = {
+        db_user:body.db_user,
+        db_name:body.db_name
+    };
 
+    Admin.getBucketList(data, function(err, list){
+        if(err) return res.json(err)
+        return res.json(list);
+    });
 }
 
 /**
